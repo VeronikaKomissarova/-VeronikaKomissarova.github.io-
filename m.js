3d5c77fe47fa4b0c1fa1f6fbf2338a0a1b9bbac5
VeronikaKomissarova <63323976+VeronikaKomissarova@users.noreply.github.com>
2021-08-04 16:20:12 +0500

    Add files via upload

---
+++ m.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+		const inputElement = document.querySelector('input') 
+		const maskOptions = { 
+		  mask: '+{7}(000)000-00-00' 
+		}
+		IMask(inputElement, maskOptions) 
+	  
+	  })
